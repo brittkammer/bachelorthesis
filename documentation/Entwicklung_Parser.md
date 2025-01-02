79061b0cb8deb4787bddfc8f1f4f4dc97c5310f0
brittkammer <kammer.britt@web.de>
2025-01-02 14:05:04 +0100

    Add new parser.py

--- documentation/Entwicklung_Parser.md
+++ documentation/Entwicklung_Parser.md
@@ -13,3 +13,8 @@
 - extrahieren der Beziehungen (umfasst Kardinalitäten)
 - speichern in interner Repräsentation 
 
+# Notizen
+- regex muss für jedes Muster einzeln angepasst werden, damit keine Fehler auftreten 
+- so gibt es für die Knoten erstmal 3 regex muster --> alle haben die gleichen gruppen das vereinfacht das extrahieren später 
+- für die Kanten ist das etwas schwieriger, die müssen in 2 gruppen geteilt werden 
+
--- dsl/parser.py
+++ dsl/parser.py
@@ -1,26 +1,67 @@
-from lark import Lark
+import re 
+import networkx as nx 
 
-grammar = """
-    start: entitaet+ relationship*
 
-    entitaet: NAME "(" attribute ("," attribute)* ")"
-    attribute: "*" NAME | "[" NAME "]" | NAME
-    relationship: NAME "--" cardinalitaet "--" NAME "--" cardinalitaet "--" NAME
-    cardinalitaet: "(" NUMBER "," NUMBER ")"
+def parse_mermaid_text(mermaid_text):
+    #regex_muster_knoten = r"(\w+)---(\w+)\(\[(?:(?:`?<ins>(.*?)<\/ins>`?)|([^\]]*?))\]\)|\((\w+)---(\w+)\(\(\((.*?)\)\)\)" 
+    regex_muster_knoten = [
+        r"(\w+)---(\w+)\(\[\"`?<ins>(.*?)<\/ins>`?\"\]\)",  # Mit <ins>-Tags - Primärschlüssel
+        r"(\w+)---(\w+)\(\[(.*?)\]\)",                     # Ohne Tags - normales Attribut
+        r"(\w+)---(\w+)\(\(\((.*?)\)\)\)"                  # Verschachtelte Klammern - zusammengesetztes Attribut
+    ]
+
+    regex_muster_kanten = [
+        r"(\w+)\{(\w+)\}--\((\d,\*|\d,\d|\*?,\d)\)---(\w+)", # Relationship{}--()---Entiät
+        r"(\w+)--\((\d,\*|\d,\d|\*?,\d)\)---(\w+)\{(\w+)\}" # Entität--()---Relationship{}
+    ]
+
+    graph = nx.DiGraph()
+    lines = mermaid_text.split("\n") 
+
+    for line in lines: 
+        # Hinzufügen der Knoten
+        for muster in regex_muster_knoten:
+            matches = re.findall(muster, line)
+            for entitaet, attribut_id, attribut_name in matches:
+                graph.add_node(entitaet, type="Entität")
+                graph.add_node(attribut_id, type="Attribut", label=attribut_name)
+                graph.add_edge(entitaet, attribut_id, Beziehung="hat Attribut")
+
+        # Hinzufügen der Kanten
+        matches = re.findall(regex_muster_kanten[0], line) 
+        for relationship,relation_name, cardinalitaet, entitaet in matches: 
+            graph.add_node(relationship, type="Relationship"),
+            graph.add_edge(relationship, entitaet, Kardinalität=cardinalitaet)
+        matches = re.findall(regex_muster_kanten[1], line)
+        for entitaet, cardinalitaet, relationship, relationship_name in matches: 
+            graph.add_node(relationship, type="Relationship"),
+            graph.add_edge(entitaet, relationship, Kardinalität=cardinalitaet)
+    return graph 
     
-    %import common.CNAME -> NAME
-    %import common.NUMBER
-    %import common.WS
-    %ignore WS
+mermaid_text =  """mermaid
+flowchart
+        Produzent---P1(["`<ins>ProdId</ins>`"])
+        Produzent---P2([Name])
+        Produzent---P3(((Zertifikate)))
+        Bauteil---B1(["`<ins>Name</ins>`"])
+        Bauteil---B2([Gewicht])
+        Bauteil---B3([Größe])
+        B3---B4([Länge])
+        B3---B5([Breite])
+        B3---B6([Höhe])
+        Produzent--(1,*)---herstellen{herstellen}
+        herstellen--(1,1)---Bauteil 
+        herstellen---H1([Jahr])
+        Bauteil--(0,*)---bestehen_aus{bestehen_aus}
+        bestehen_aus{bestehen_aus}--(0,*)---Bauteil 
 """
 
-parser = Lark(grammar)
+graph = parse_mermaid_text(mermaid_text)
 
-# Beispiel für Parsing
-input_text = """
-Produzent (*ProdId, Name, [Zertifikate])
-Bauteil (*Name, Gewicht, Größe(Länge, Breite, Höhe))
-Produzent -- (1,*) -- herstellen (Jahr) -- (1,1) -- Bauteil
-"""
-tree = parser.parse(input_text)
-print(tree.pretty())
+print("Knoten:")
+for node, data in graph.nodes(data=True):
+    print(node, data)
+
+print("\nKanten:")
+for u, v, data in graph.edges(data=True):
+    print(u, v, data)
