f66204d3115c667ae789deb4ef77336a290aaa07
brittkammer <kammer.britt@web.de>
2025-02-12 14:14:08 +0100

    fehler und fehler_visualisierung nicht mehr als global sondern nur in compare_graphs()

--- app/graphMatcher.py
+++ app/graphMatcher.py
@@ -1,45 +1,9 @@
-from app.parse_into_graph import parse_mermaid_text
-from app.solution_parser import parse_solution
 import networkx as nx 
 from itertools import product
 
 
 ####################### FEHLERÜBERSICHT #################
-global fehler, fehler_visualisierung
-fehler = {
-# Fehler bei Knoten
-    "richtige_Knoten": [],
-    "falsche_Nachbarn": [],
-    "fehlende_Knoten": [],
-    "extra_Knoten": [],
-    "falscher_Typ_Knoten": [], #typabweichung
-    "falscher_Name_Knoten":[], # label
-    "helper-liste-falscher-Name": [], # liste mit falschen Namen und "richtigem" Namen / ID
-    "falscher_Knoten": [], # weder label, type noch Nachbarn stimmen
-    "NichteinhaltungERM-Regeln": [],
-# Fehler bei Kanten
-    "fehlende_Kanten": [],
-    "extra_Kanten": [],
-    "falsche_Kanten": [] # Kardinalität, Beziehung?
 
-}
-
-fehler_visualisierung= {
-    "richtige_Knoten": [],
-    "falsche_Nachbarn_gelb": [],
-    "fehlende_Knoten_Info": [],
-    "extra_Knoten_gelb": [],
-    "falscher_Typ_Knoten_rot": [],
-    "falscher_Name_Knoten_rot": [],
-    "falscher_Knoten_rot": [],
-    "NichteinhaltungERM-Regeln_Info": [],
-# Kanten
-    "fehlende_Kanten_Info": [],
-    "extra_Kanten_gelb": [],
-    "falsche_Kanten_rot": [],
-    "richtige_Kanten_grün": []
-    
-}
 ###################### FEHLER TEMPLATES #############################
 
 # richtigeKnotenGRÜN = f"   style {studentNode} fill:#d4edda,stroke:#5a8f66,stroke-width:2px"
@@ -53,18 +17,51 @@ fehler_visualisierung= {
 #################### MATCHER #########################
 
 def compare_graphs(musterGraph, studentenGraph, studentische_loesung):
+    fehler = {
+    # Fehler bei Knoten
+        "richtige_Knoten": [],
+        "falsche_Nachbarn": [],
+        "fehlende_Knoten": [],
+        "extra_Knoten": [],
+        "falscher_Typ_Knoten": [], #typabweichung
+        "falscher_Name_Knoten":[], # label
+        "helper-liste-falscher-Name": [], # liste mit falschen Namen und "richtigem" Namen / ID
+        "falscher_Knoten": [], # weder label, type noch Nachbarn stimmen
+        "NichteinhaltungERM-Regeln": [],
+    # Fehler bei Kanten
+        "fehlende_Kanten": [],
+        "extra_Kanten": [],
+        "falsche_Kanten": [] # Kardinalität, Beziehung?
+
+    }
+
+    fehler_visualisierung= {
+        "richtige_Knoten": [],
+        "falsche_Nachbarn_gelb": [],
+        "fehlende_Knoten_Info": [],
+        "extra_Knoten_gelb": [],
+        "falscher_Typ_Knoten_rot": [],
+        "falscher_Name_Knoten_rot": [],
+        "falscher_Knoten_rot": [],
+        "NichteinhaltungERM-Regeln_Info": [],
+    # Kanten
+        "fehlende_Kanten_Info": [],
+        "extra_Kanten_gelb": [],
+        "falsche_Kanten_rot": [],
+        "richtige_Kanten_grün": []
+    }
     for studentNode, studentData in studentenGraph.nodes(data=True):
             if studentNode in fehler['richtige_Knoten']:
                 continue
             if studentData['type'] == 'Entität':
                 # ERM MODELLIERUNGS REGELN PRÜFEN 
-                entitätenPrüfen(musterGraph, studentenGraph, studentNode, studentData)
-                kardinalitätPrüfen(musterGraph, studentenGraph, studentNode, studentData)
+                entitätenPrüfen(musterGraph, studentenGraph, studentNode, studentData, fehler, fehler_visualisierung)
+                kardinalitätPrüfen(musterGraph, studentenGraph, studentNode, studentData, fehler_visualisierung)
             elif studentData['type'] == 'Relationship':
-                relationshipsPrüfen(musterGraph, studentenGraph, studentNode, studentData)
-                kardinalitätPrüfen(musterGraph, studentenGraph, studentNode, studentData)
+                relationshipsPrüfen(musterGraph, studentenGraph, studentNode, studentData, fehler, fehler_visualisierung)
+                kardinalitätPrüfen(musterGraph, studentenGraph, studentNode, studentData, fehler_visualisierung)
             elif 'Attribut' in studentData['type']:
-                attributePrüfen(musterGraph, studentenGraph, studentNode, studentData)
+                attributePrüfen(musterGraph, studentenGraph, studentNode, studentData, fehler, fehler_visualisierung)
 
     if musterGraph.number_of_nodes() > studentenGraph.number_of_nodes(): 
         studentische_loesung = studentische_loesung + f"\n   fehlerFehlendeKnoten[Fehler: Es fehlen noch Enitäten, Attribute oder Relationships!] \n    style fehlerFehlendeKnoten fill:#fde2e1,stroke:#b91c1c,stroke-width:2p,font-weight:bold;"
@@ -75,7 +72,7 @@ def compare_graphs(musterGraph, studentenGraph, studentische_loesung):
     feedback = visualisieren(fehler_visualisierung, studentische_loesung)
     return feedback
 
-def elementPrüfen(MGraph, SGraph, studentNode, studentData): 
+def elementPrüfen(MGraph, SGraph, studentNode, studentData, fehler, fehler_visualisierung): 
     fehlerdict = {
         "richtige_Knoten": [],
         "falsche_Nachbarn": [],
@@ -127,7 +124,7 @@ def elementPrüfen(MGraph, SGraph, studentNode, studentData):
         fehler_visualisierung["falscher_Knoten_rot"].append(f"   style {studentNode} fill:#F4CCCC,stroke:#CC0000,color:#CC0000,stroke-width:2px,font-weight:bold")
         return
 
-def kardinalitätPrüfen(MGraph, SGraph, studentNode, studentData): 
+def kardinalitätPrüfen(MGraph, SGraph, studentNode, studentData, fehler_visualisierung): 
     fehlerdict = {
         "richtigeKanten": [],
         "falscheKanten": []
@@ -180,7 +177,7 @@ def kardinalitätPrüfen(MGraph, SGraph, studentNode, studentData):
 
 
 
-def entitätenPrüfen(MGraph, SGraph, studentNode, studentData):
+def entitätenPrüfen(MGraph, SGraph, studentNode, studentData, fehler, fehler_visualisierung):
     # Übersicht der Nachbarn erstellen (hier sind labels egal da ich auf type prüfe)
     studentenNachbarn = sorted(list(SGraph.neighbors(studentNode))) + sorted(list(SGraph.predecessors(studentNode)))
     anzahlPrimärschlüssel = 0 # es darf nur ein Primärschlüssel geben
@@ -192,13 +189,13 @@ def entitätenPrüfen(MGraph, SGraph, studentNode, studentData):
             booleanEntitäten = True
     if anzahlPrimärschlüssel == 1 and booleanEntitäten == False: 
         # elementPrüfen(MGraph, musterNode, musterData, SGraph, studentNode, studentData)
-        elementPrüfen(MGraph, SGraph, studentNode, studentData)
+        elementPrüfen(MGraph, SGraph, studentNode, studentData, fehler, fehler_visualisierung)
     else: 
         fehler["NichteinhaltungERM-Regeln"].append(studentNode)
         fehler_visualisierung["NichteinhaltungERM-Regeln_Info"].append(f"   style {studentNode} fill:#FFD966,stroke:#FFA500,stroke-width:2px") ########## HINZUFÜGEN
 
 
-def relationshipsPrüfen(MGraph, SGraph, studentNode, studentData):
+def relationshipsPrüfen(MGraph, SGraph, studentNode, studentData, fehler, fehler_visualisierung):
     # Übersicht der Nachbarn 
     studentenNachbarn = sorted(list(SGraph.neighbors(studentNode))) + sorted(list(SGraph.predecessors(studentNode)))
     anzahlPrimärschlüssel = 0 # es darf keine primärschlüssel-attribute besitzen
@@ -213,21 +210,21 @@ def relationshipsPrüfen(MGraph, SGraph, studentNode, studentData):
             booleanRelationships = True
     if anzahlPrimärschlüssel == 0 and anzahlZusammengesetzteAttribute == 0 and booleanRelationships == False: 
         # elementPrüfen(MGraph, musterNode, musterData, SGraph, studentNode, studentData)
-        elementPrüfen(MGraph, SGraph, studentNode, studentData)
+        elementPrüfen(MGraph, SGraph, studentNode, studentData, fehler, fehler_visualisierung)
 
     else: 
         fehler["NichteinhaltungERM-Regeln"].append(studentNode)
         fehler_visualisierung["NichteinhaltungERM-Regeln_Info"].append(f"   style {studentNode} fill:#FFD966,stroke:#FFA500,stroke-width:2px") ########## HINZUFÜGEN
 
 
-def attributePrüfen(MGraph, SGraph, studentNode, studentData): 
+def attributePrüfen(MGraph, SGraph, studentNode, studentData, fehler, fehler_visualisierung): 
     studentenNachbarn = sorted(list(SGraph.neighbors(studentNode))) + sorted(list(SGraph.predecessors(studentNode)))
     if studentData['type'] == 'Attribut' or studentData['type'] == 'mehrwertiges Attribut': 
         # Attribut kann nur an einem Element liegen (nicht an Attribut und auch nicht an mehrwertiges Attribut und auch nicht Primärschlüssel)
         for nachbar in studentenNachbarn: 
             if len(studentenNachbarn) == 1 and SGraph.nodes[nachbar]['type'] != 'Attribut' and SGraph.nodes[nachbar]['type'] != 'mehrwertiges Attribut' and SGraph.nodes[nachbar]['type'] != 'Primärschlüssel-Attribut': 
                 # elementPrüfen(MGraph, musterNode, musterData, SGraph, studentNode, studentData)
-                elementPrüfen(MGraph, SGraph, studentNode, studentData)
+                elementPrüfen(MGraph, SGraph, studentNode, studentData, fehler, fehler_visualisierung)
 
             else: 
                 fehler["NichteinhaltungERM-Regeln"].append(studentNode)
@@ -237,7 +234,7 @@ def attributePrüfen(MGraph, SGraph, studentNode, studentData):
         for nachbar in studentenNachbarn:  
             if SGraph.nodes[nachbar]['type'] == 'Entität' or SGraph.nodes[nachbar]['type'] == 'zusammengesetztes Attribut' and len(studentenNachbarn) == 1: 
                 # elementPrüfen(MGraph, musterNode, musterData, SGraph, studentNode, studentData)
-                elementPrüfen(MGraph, SGraph, studentNode, studentData)
+                elementPrüfen(MGraph, SGraph, studentNode, studentData, fehler, fehler_visualisierung)
 
             else: 
                 fehler["NichteinhaltungERM-Regeln"].append(studentNode)
@@ -257,7 +254,7 @@ def attributePrüfen(MGraph, SGraph, studentNode, studentData):
         # if anzahlPrimärschlüssel == 0 and anzahlMehrwertigeAttribute == 0 and anzahlZusammengesetztesAttribut == 0: 
         if anzahlMehrwertigeAttribute == 0 and anzahlZusammengesetztesAttribut == 0: 
             # elementPrüfen(MGraph, musterNode, musterData, SGraph, studentNode, studentData)
-            elementPrüfen(MGraph, SGraph, studentNode, studentData)
+            elementPrüfen(MGraph, SGraph, studentNode, studentData, fehler, fehler_visualisierung)
 
         else: 
             fehler["NichteinhaltungERM-Regeln"].append(studentNode)
